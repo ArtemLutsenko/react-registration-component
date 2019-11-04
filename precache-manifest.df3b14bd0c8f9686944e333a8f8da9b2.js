881daa72fd848489622c53740781cf3ed0fbd978
ArtemLutsenko <artem.lutsenko@gmail.com>
2019-11-04 12:59:06 +0200

    Updates

---
+++ precache-manifest.df3b14bd0c8f9686944e333a8f8da9b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9184c65a24245c42515541cc3157a699",
+    "url": "/github.io/react-registration-component/index.html"
+  },
+  {
+    "revision": "155d5ef8d1f5253c0890",
+    "url": "/github.io/react-registration-component/static/css/main.32479f11.chunk.css"
+  },
+  {
+    "revision": "dcd009de6284d4c5df50",
+    "url": "/github.io/react-registration-component/static/js/2.06e203ff.chunk.js"
+  },
+  {
+    "revision": "155d5ef8d1f5253c0890",
+    "url": "/github.io/react-registration-component/static/js/main.c9b26d87.chunk.js"
+  },
+  {
+    "revision": "2d5fc5a9365c77c591fa",
+    "url": "/github.io/react-registration-component/static/js/runtime-main.72d2892f.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/github.io/react-registration-component/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/github.io/react-registration-component/static/media/shopping-bag.30f07071.svg"
+  }
+]);
